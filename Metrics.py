91ea62fb1f34700ef08872de77f4f5bc1fb73bce
Javier <javiersgjavi@gmail.com>
2021-10-09 18:36:18 +0200

    Added DiversityMaximunExtension metric

--- Metrics.py
+++ Metrics.py
@@ -80,3 +80,37 @@ class DiversitySpacing:
         return np.sqrt(
                 np.sum(np.power(np.subtract(minDistances, meanDistance), 2)) /
                 len(minDistances))
+                
+
+class DiversityMaximunExtension:
+    def __init__(self):
+        pass
+
+    def runMetric(self, statistics):
+        points = []
+
+        for point in statistics.ParetoSet:
+            p = np.array([point.Fitness[0], point.Fitness[0]])
+            points.append(p)
+
+        points = np.array(points)
+
+        # Get max and min fitness for function
+        maxim_points = []
+        min_points = []
+        for i in range(points.shape[1]):
+            index_max = np.where(points==np.max(points[:,i]))
+            index_min = np.where(points==np.max(points[:,i]))
+            
+            maxim_points.append(points[index_max])
+            min_points.append(points[index_min])
+
+        # Calculate metric
+        count = 0
+        for i in range(len(maxim_points)):
+            count += (maxim_points[i] - min_points[i])[0]**2
+
+        return np.sqrt(count)
+
+        
+        
